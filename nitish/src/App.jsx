1d8422e213091b7f302b59f4422cfe1b5993d7ba
2222nit <nitishkamboj8070@gmail.com>
2026-03-20 11:18:16 +0530

    asd

--- nitish/src/App.jsx
+++ nitish/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import About from './Components/About'
-import Resume from './Components/Resume'
-import Footer from './Components/Footer'
+// import Hero from './Components/Hero'
+// import About from './Components/About'
+// import Resume from './Components/Resume'
+// import Footer from './Components/Footer'
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <About />
       <Resume />
-      <Footer />
+      <Footer /> */}
     </div>
   )
 }
